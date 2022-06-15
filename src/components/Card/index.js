9ad08d2b82d1b61739f47faa00181aacbd1ef63b
rcloureiro33 <roberto.cloureiro@gmail.com>
2022-06-15 13:40:55 -0300

    inicio

---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+export function Card() {
+    return (
+        <div className="body">
+            <img src="https://metech.nyc3.digitaloceanspaces.com/site-carmopolis/icon_dashboard_galeriadeimagens_256_256.png" alt="photo" />
+            <h1>TEXTO</h1>
+        </div>
+    );      
+}
